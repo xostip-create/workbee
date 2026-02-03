752cbe8633b7b8f108c427d15d986f5070c30a7f
Emmanuel Solomon <xostip@gmail.com>
2026-02-03 19:07:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6723010539-c07e2",
+  "appId": "1:642688924533:web:caa7b8a69d2e424696713c",
+  "apiKey": "AIzaSyCHOmasQ-qIx6Jm0IhLmrPCYvM2gM-Xpwo",
+  "authDomain": "studio-6723010539-c07e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "642688924533"
+};
